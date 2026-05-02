d127f9f83b3dab773a953bec3d1acb141fc74f8c
Rupesh Lakshman <codewithruppu@gmail.com>
2026-05-02 15:51:10 +0530

    health

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.urls import path, include
+
+def health(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path("", include("oidc.urls")),
+    path("/health", health)
 ]
